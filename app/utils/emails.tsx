9329967e9bcc0848d157189ab7ee96f6e6491bfc
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-10-15 07:44:44 +0200

    update email

--- app/utils/emails.tsx
+++ app/utils/emails.tsx
@@ -17,21 +17,34 @@ export function newShopEmail() {
     "New Bakery",
     `Hello, and thanks for your interest!
 
-For the bakery, we're interested in the google map link because it got all the adresse-email-phone-opening hours-coordinates we need.
+For the bakery, we're interested in the Google Maps link because it provides all the information we need: address, email, phone number, opening hours, and coordinates.
 Please paste it here below:
 
 
-For the ${chocolatineName}, You can fill any of these fields, with a note from 0 to 5
--Softness/Moelleux:
--Flakiness/Feuilletage:
--Crispiness/Croustillant:
--Fondant:
--Chocolate quality:
--Chocolate disposition:
--Note:
--Visual aspect:
+For the ${chocolatineName}, you can fill any of these fields with a value from -2 to 2, based on the provided criteria.
 
-If you know the ingredients, you can add them here too
+-Light or Dense:
+  (-2 is for very light, -1 for light, 0 for medium, 1 for dense, 2 for very dense)
+
+-Flaky or Brioche:
+  (-2 is for very flaky, -1 for flaky, 0 for medium, 1 for brioche, 2 for very brioche)
+
+-Buttery:
+  (-2 is for not buttery at all, -1 for not buttery, 0 for medium, 1 for buttery, 2 for very buttery)
+
+-Golden or Pale:
+  (-2 is for very golden, -1 for golden, 0 for medium, 1 for pale, 2 for very pale)
+
+-Crispy or Soft:
+  (-2 is for very crispy, -1 for crispy, 0 for medium, 1 for soft, 2 for very soft)
+
+-Chocolate Disposition:
+  (-2 for superimposed, -1 for stuck side by side, 0 for well-distributed, 1 for too far away, 2 for on the edges)
+
+-Good or Not Good:
+  (-2 for very bad, 2 for very good)
+
+If you know the ingredients, you can add them here too.
 
 Thanks!
 Arnaud, from Kiss My Chocolatine
