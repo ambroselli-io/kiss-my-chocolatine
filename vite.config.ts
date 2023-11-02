82e5aa9237892fcc6e0c1d02ff672360afd5055d
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-11-02 15:33:29 +0100

    fix: port

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
