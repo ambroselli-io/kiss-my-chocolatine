bc2350e25666aeaf886b25fc53efc2205aabb64d
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-11-04 15:50:57 +0100

    fuck no google link

--- app/utils/parseGoogleLinkForCoordinates.server.js
+++ app/utils/parseGoogleLinkForCoordinates.server.js
@@ -5,6 +5,12 @@ export default async function parseGoogleLinkForCoordinates(rawGoogleLink) {
   // -> https://maps.app.goo.gl/2PScR6bSNXJUyns57
   // ->https://www.google.com/maps/place/Le+Pain+Retrouvé/@48.8777186,2.3396138,17z/data=!3m2!4b1!5s0x47e66e473872ba6b:0xf7d926070e69fc39!4m6!3m5!1s0x47e66f897294d69b:0x37816e98cb091727!8m2!3d48.8777186!4d2.3396138!16s%2Fg%2F11qqj676ry?entry=ttu
 
+  if (!rawGoogleLink?.length > 0) {
+    return {
+      latitude: null,
+      longitude: null,
+    };
+  }
   const coordintesRegexp =
     /@[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)?/g;
   let coordinates = rawGoogleLink.match(coordintesRegexp);
