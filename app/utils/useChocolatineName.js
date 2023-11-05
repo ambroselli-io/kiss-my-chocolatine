ea355ec1d8156c31125d1a8228e1c98c0c838ce4
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-11-05 10:14:22 +0100

    hotfix

--- app/utils/useChocolatineName.js
+++ app/utils/useChocolatineName.js
@@ -1,28 +1,14 @@
 import Cookies from "js-cookie";
-import React, { useState, useEffect } from "react";
 
 export default function useChocolatineName() {
+  const chocolatineName = Cookies.get("chocolatine-name") || "chocolatine";
+  const chocolatinesName = Cookies.get("chocolatines-name") || "chocolatines";
+
   const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
-
-  const [chocolatineName, setChocolatineName] = useState("pain au chocolat");
-  const [chocolatinesName, setChocolatinesName] = useState("pains au chocolat");
-  const [newAppName, setNewAppName] = useState("Kiss\u00A0My\u00A0Chocolatine");
-
-  useEffect(() => {
-    const newChocolatineName = Cookies.get("chocolatine-name");
-    if (!!newChocolatineName) {
-      setChocolatineName(newChocolatineName);
-      setChocolatinesName(
-        Cookies.get("chocolatines-name") || "pains au chocolat",
-      );
-      setNewAppName(
-        `Kiss\u00A0My\u00A0${newChocolatineName
-          .split(" ")
-          .map(capitalize)
-          .join("\u00A0")}`,
-      );
-    }
-  }, [Cookies.get("chocolatine-name")]);
+  const newAppName = `Kiss\u00A0My\u00A0${chocolatineName
+    .split(" ")
+    .map(capitalize)
+    .join("\u00A0")}`;
 
   return {
     chocolatineName,
